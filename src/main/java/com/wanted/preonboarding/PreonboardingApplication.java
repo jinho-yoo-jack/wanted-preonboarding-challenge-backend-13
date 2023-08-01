48725a87bc0e42d67f4c60cf169a4892e7203869
유진호 <black@yujinhoui-MacBookPro.local>
2023-08-01 16:46:56 +0900

    first commit

---
+++ src/main/java/com/wanted/preonboarding/PreonboardingApplication.java
@@ -0,0 +1,13 @@
+package com.wanted.preonboarding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PreonboardingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PreonboardingApplication.class, args);
+    }
+
+}
