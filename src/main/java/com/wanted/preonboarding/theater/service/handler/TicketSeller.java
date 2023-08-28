414831a781619a8b6d1fcf7a171485ca525c8062
Bak jun geon <pjg3239@naver.com>
2023-08-28 21:26:19 +0900

    feat: ticketSeller 티켓 교환 메소드 추가

--- src/main/java/com/wanted/preonboarding/theater/service/handler/TicketSeller.java
+++ src/main/java/com/wanted/preonboarding/theater/service/handler/TicketSeller.java
@@ -3,11 +3,24 @@ package com.wanted.preonboarding.theater.service.handler;
 public class TicketSeller {
     private final TicketOffice ticketOffice;
 
-    public TicketSeller(TicketOffice ticketOffice){
+    public TicketSeller(TicketOffice ticketOffice) {
         this.ticketOffice = ticketOffice;
     }
 
-    public TicketOffice getTicketOffice(){
+    public TicketOffice getTicketOffice() {
         return ticketOffice;
+    }
+
+    public void sellTicket(Audience audience) {
+        Ticket ticket = ticketOffice.getTicket();
+        Long ticketFee = ticket.getFee();
+        audience.payFee(ticketFee);
+        ticketOffice.plusAmount(ticketFee);
+        audience.receiveTicket(ticket);
+    }
+
+    public void convertInvitationToTicket(Audience audience) {
+        Ticket ticket = ticketOffice.getTicket();
+        audience.receiveTicket(ticket);
     }
 }
