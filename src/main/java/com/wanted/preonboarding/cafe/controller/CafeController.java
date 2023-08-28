9200b4524ad0afacc6edcda14e065bf4352c5108
Bak jun geon <pjg3239@naver.com>
2023-08-29 03:46:39 +0900

    refactor: 음료와 수량 RequestBody 받는 부분 생성

--- src/main/java/com/wanted/preonboarding/cafe/controller/CafeController.java
+++ src/main/java/com/wanted/preonboarding/cafe/controller/CafeController.java
@@ -2,10 +2,14 @@ package com.wanted.preonboarding.cafe.controller;
 
 import com.wanted.preonboarding.cafe.service.CafeService;
 import lombok.RequiredArgsConstructor;
+
+// import org.hibernate.mapping.List;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.wanted.preonboarding.cafe.service.Dtos.*;
 import java.util.HashMap;
 
 @RestController
@@ -15,14 +19,14 @@ public class CafeController {
     private final CafeService cafeService;
 
     @GetMapping("hello")
-    public String welcomeMessage(){
+    public String welcomeMessage() {
         return "Welcome to The Wanted coding cafe!!";
     }
 
     @GetMapping("order")
-    public String orderFromMenu(){
+    public String orderFromMenu(@RequestBody RequestOrders orders) {
         HashMap<String, Integer> menu = new HashMap<String, Integer>();
-        menu.put("AMERICANO", 3);
+        menu.put(orders.getName(), orders.getQuantity());
         return cafeService.orderFrom(menu);
     }
 }
---
+++ src/main/java/com/wanted/preonboarding/cafe/service/Dtos/RequestOrders.java
@@ -0,0 +1,13 @@
+
+package com.wanted.preonboarding.cafe.service.Dtos;
+
+import lombok.Getter;
+
+
+public class RequestOrders {
+    @Getter
+    private String name;
+    @Getter
+    private Integer quantity;
+}
+
