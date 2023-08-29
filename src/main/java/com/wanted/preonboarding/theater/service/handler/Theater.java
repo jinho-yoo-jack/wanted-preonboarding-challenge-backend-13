329d9eb34e9d39589f902e15132615eca54d901f
leekyuwon <kyuwoon369@gmail.com>
2023-08-29 19:58:17 +0900

    티켓 판매원에게 판매를 넘겨라

--- src/main/java/com/wanted/preonboarding/theater/service/handler/Theater.java
+++ src/main/java/com/wanted/preonboarding/theater/service/handler/Theater.java
@@ -7,15 +7,7 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class Theater {
 
-    public void enter(Audience audience, TicketSeller ticketSeller){
-        if(audience.getBag().hasInvitation()){
-            Ticket ticket = ticketSeller.getTicketOffice().getTicket();
-            audience.getBag().setTicket(ticket);
-        }else {
-            Ticket ticket = ticketSeller.getTicketOffice().getTicket();
-            audience.getBag().minusAmount(ticket.getFee());
-            ticketSeller.getTicketOffice().plusAmount(ticket.getFee());
-            audience.getBag().setTicket(ticket);
-        }
+    public void enter(Audience audience, TicketSeller ticketSeller) {
+        ticketSeller.sellTo(audience);
     }
 }
--- src/main/java/com/wanted/preonboarding/theater/service/handler/TicketSeller.java
+++ src/main/java/com/wanted/preonboarding/theater/service/handler/TicketSeller.java
@@ -3,11 +3,19 @@ package com.wanted.preonboarding.theater.service.handler;
 public class TicketSeller {
     private final TicketOffice ticketOffice;
 
-    public TicketSeller(TicketOffice ticketOffice){
+    public TicketSeller(TicketOffice ticketOffice) {
         this.ticketOffice = ticketOffice;
     }
 
-    public TicketOffice getTicketOffice(){
-        return ticketOffice;
+    public void sellTo(Audience audience) {
+        if (audience.getBag().hasInvitation()) {
+            Ticket ticket = ticketOffice.getTicket();
+            audience.getBag().setTicket(ticket);
+        } else {
+            Ticket ticket = ticketOffice.getTicket();
+            audience.getBag().minusAmount(ticket.getFee());
+            ticketOffice.plusAmount(ticket.getFee());
+            audience.getBag().setTicket(ticket);
+        }
     }
 }
