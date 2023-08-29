8663ee0f69f434a78399c1e7139c081ab9d20abf
kim-jingyu <swc05161@gmail.com>
2023-08-29 20:45:18 +0900

    feat(과제2): CafeService 생성

--- src/main/java/com/wanted/preonboarding/cafe/service/CafeService.java
+++ src/main/java/com/wanted/preonboarding/cafe/service/CafeService.java
@@ -1,24 +1,22 @@
 package com.wanted.preonboarding.cafe.service;
 
+import com.wanted.preonboarding.cafe.dto.CafeRequest;
 import com.wanted.preonboarding.cafe.service.handler.Cafe;
 import com.wanted.preonboarding.cafe.service.handler.Cashier;
 import com.wanted.preonboarding.cafe.service.handler.Customer;
+import com.wanted.preonboarding.cafe.service.handler.OrderTable;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
 public class CafeService {
     private final Cafe wantedCafe;
 
-    public String orderFrom(HashMap<String, Integer> menu){
+    public String orderFrom(CafeRequest cafeRequest){
         Cashier cashier = new Cashier(wantedCafe);
-        Map<String, Integer> myOrders = new HashMap<>();
-        myOrders.put("AMERICANO", 3);
-        Customer c1 = new Customer("Card", myOrders);
+        OrderTable orderTable = new OrderTable(cafeRequest.getOrders());
+        Customer c1 = new Customer("Card", orderTable);
         return c1.buyCoffee(cashier);
     }
 }
