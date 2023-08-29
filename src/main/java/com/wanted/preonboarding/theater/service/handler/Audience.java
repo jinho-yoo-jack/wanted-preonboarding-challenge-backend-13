f29f3305c0707052c6211435a44ba08b56f757c6
leekyuwon <kyuwoon369@gmail.com>
2023-08-29 22:01:38 +0900

    초대권을 확인하고 티켓을 구매하는 로직을 Audience로 옮겨라

--- src/main/java/com/wanted/preonboarding/theater/service/handler/Audience.java
+++ src/main/java/com/wanted/preonboarding/theater/service/handler/Audience.java
@@ -6,6 +6,14 @@ public class Audience {
     public Audience(Bag bag){
         this.bag = bag;
     }
-
-    public Bag getBag(){ return bag;}
+    public Long buy(Ticket ticket){
+        if (bag.hasInvitation()) {
+            bag.setTicket(ticket);
+            return 0L;
+        } else {
+            bag.setTicket(ticket);
+            bag.minusAmount(ticket.getFee());
+            return ticket.getFee();
+        }
+    }
 }
--- src/main/java/com/wanted/preonboarding/theater/service/handler/TicketSeller.java
+++ src/main/java/com/wanted/preonboarding/theater/service/handler/TicketSeller.java
@@ -8,14 +8,7 @@ public class TicketSeller {
     }
 
     public void sellTo(Audience audience) {
-        if (audience.getBag().hasInvitation()) {
             Ticket ticket = ticketOffice.getTicket();
-            audience.getBag().setTicket(ticket);
-        } else {
-            Ticket ticket = ticketOffice.getTicket();
-            audience.getBag().minusAmount(ticket.getFee());
-            ticketOffice.plusAmount(ticket.getFee());
-            audience.getBag().setTicket(ticket);
-        }
+            ticketOffice.plusAmount(audience.buy(ticket));
     }
 }
