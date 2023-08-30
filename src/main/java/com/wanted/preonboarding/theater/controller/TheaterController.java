1db645b77a37c7277b4c602868cb99752ff3fdf8
강수나 <acrnm148@gmail.com>
2023-08-31 07:52:41 +0900

    [과제1] Theater 리팩토링

--- src/main/java/com/wanted/preonboarding/theater/controller/TheaterController.java
+++ src/main/java/com/wanted/preonboarding/theater/controller/TheaterController.java
@@ -2,26 +2,49 @@ package com.wanted.preonboarding.theater.controller;
 
 import com.wanted.preonboarding.cafe.service.CafeService;
 import com.wanted.preonboarding.theater.service.TheaterService;
+import com.wanted.preonboarding.theater.service.handler.Audience;
+import com.wanted.preonboarding.theater.service.handler.AudienceRequest;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 
+@Slf4j
 @RestController
 @RequestMapping("/theater")
 @RequiredArgsConstructor
 public class TheaterController {
     private final TheaterService theaterService;
 
-    @GetMapping("hello")
-    public String welcomeMessage(){
-        return "Welcome to The Wanted Theater";
+    /**
+     * Wanted Theater 환영 메시지
+     * @return String
+     */
+    @GetMapping("/hello")
+    public ResponseEntity<String> welcomeMessage(){
+        log.info("TheaterController.welcomeMessage");
+
+        String response = "Welcome to The Wanted Theater";
+        return ResponseEntity.ok(response);
     }
 
-    @GetMapping("enter")
-    public String enter(){
-        return theaterService.enter();
+    /**
+     * Wanted Theater 입장
+     * @return String
+     */
+    @GetMapping("/enter")
+    public ResponseEntity<String> enter(@RequestBody AudienceRequest audienceRequest){
+        log.info("TheaterController.enter: {}", audienceRequest);
+
+        String response = theaterService.enter(Audience.builder()
+                .bag(audienceRequest.getBag())
+                .build());
+        return ResponseEntity.ok(response);
     }
 }
--- src/main/java/com/wanted/preonboarding/theater/service/handler/TicketOffice.java
+++ src/main/java/com/wanted/preonboarding/theater/service/handler/TicketOffice.java
@@ -1,25 +1,30 @@
 package com.wanted.preonboarding.theater.service.handler;
 
+import lombok.*;
+
 import java.util.Arrays;
 import java.util.List;
 
+@Builder
+@Getter
+@ToString
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
 public class TicketOffice {
     private long amount;
-    private final List<Ticket> tickets;
-
-    public TicketOffice(Long amount, Ticket ... tickets) {
-        this.amount = amount;
-        this.tickets = Arrays.asList(tickets);
-    }
-
-    public Ticket getTicket(){
-        return tickets.get(0);
-    }
+    private List<Ticket> tickets;
 
     public void minusAmount(long amount) {
         this.amount -= amount;
     }
     public void plusAmount(long amount) {
         this.amount += amount;
+    }
+
+    public Ticket getTicket(){
+        if (tickets == null) {
+            return null;
+        }
+        return tickets.get(0);
     }
 }
