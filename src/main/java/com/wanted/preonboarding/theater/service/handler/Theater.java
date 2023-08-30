b302fe9a61431be4205dce9dce69963dcf307ffc
benjaminuj <chosj1526@gmail.com>
2023-08-31 05:07:36 +0900

    refactor: Place the ticket seller creation code at the Theater class

--- src/main/java/com/wanted/preonboarding/theater/service/handler/Theater.java
+++ src/main/java/com/wanted/preonboarding/theater/service/handler/Theater.java
@@ -9,6 +9,7 @@ public class Theater {
 
     private TicketSeller ticketSeller;
     public void enter(Audience audience){
+        TicketSeller ticketSeller = new TicketSeller(new TicketOffice(20000L, new Ticket(100L)));
         ticketSeller.sellTicket(audience);
     }
 }
