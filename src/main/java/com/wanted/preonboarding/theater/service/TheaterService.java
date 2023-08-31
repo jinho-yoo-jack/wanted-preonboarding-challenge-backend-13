460871c420aa6075ec1a40921d38636792a81bae
yw824 <yongwoo25@gmail.com>
2023-08-31 21:58:57 +0900

    feat: 과제1 - RequestMessage 등 수정 - class Audience : 	- Bag을 같이 가지고 있을 필요가 없을 것 같아 Bag을 삭제하고 	  Pass, Money를 원소로 가짐 	- Pass를 설정하는 함수와 Money를 연산하는 함수 설정 - Interface Pass : 	- 모두 입장권의 개념에서 Invitation과 Ticket이 import하게끔 	  한다. - Invitation : 	- 초대권 / 있으면 TicketOffice에서 통과 - Ticket : 	- 구매한 예매권 / 있으면 TicketOffice에서 통과 	- 예매권마저 없으면 TicketOffice에서 구매하게끔 한다. 	- 돈마저 없으면 팔지 않고 TicketOffice에서 거절(return false) - class TicketOffice 	- TicketSeller 클래스가 필요없을 것 같아 삭제하고 	- 멤버변수롤 그대로 TicketOffice에서 설정 - Theater : 	- TicketOffice 그대로 가지고 있다. 	- RequestMessage에서 TicketOffice/Audience 받아 		- 본인 멤버변수로 TicketOffice 설정 		- Audience 데이터 받아 입장 결정 - TheaterService : 	- 전체 실행 파일 	- Theater 멤버변수에게 RequestMessage로 전달 - RequestMessage : 	- Theater에게 전달되는 멤버변수 	- TicketOffice와 Audience 전달

--- src/main/java/com/wanted/preonboarding/theater/service/TheaterService.java
+++ src/main/java/com/wanted/preonboarding/theater/service/TheaterService.java
@@ -3,6 +3,7 @@ package com.wanted.preonboarding.theater.service;
 import com.wanted.preonboarding.theater.service.handler.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import com.wanted.preonboarding.theater.Client.RequestMessage;
 
 @Service
 @RequiredArgsConstructor
@@ -11,8 +12,16 @@ public class TheaterService {
 
     // 정상 실행되는 지 확인
     public String enter(){
-        theater.enter(new Audience(new Bag(1000L)),
-                new TicketSeller(new TicketOffice(20000L, new Ticket(100L))));
-        return "Have a good time.";
+        if(
+            theater.enter(
+                new RequestMessage(
+                    new Audience(1000L),
+                    new TicketOffice(20000L, new Ticket(100L))
+                )
+            )
+        ) { // 100L짜리 티켓 하나 가진 TicketOffice
+            return "Have a good time.";
+        }
+        return "You don't have pass for Movie.";
     }
 }
--- src/main/java/com/wanted/preonboarding/theater/service/handler/Audience.java
+++ src/main/java/com/wanted/preonboarding/theater/service/handler/Audience.java
@@ -1,11 +1,55 @@
 package com.wanted.preonboarding.theater.service.handler;
 
-public class Audience {
-    private final Bag bag;
+import com.wanted.preonboarding.theater.service.Interface.Pass;
 
-    public Audience(Bag bag){
-        this.bag = bag;
+public class Audience {
+    private Long money;
+    private Pass pass;
+
+    public Audience() {
+        this(null, 0L);
     }
 
-    public Bag getBag(){ return bag;}
+    public Audience(Long money) {
+        this(null, money);
+    }
+
+    public Audience(Pass pass) {
+        this(pass, 0L);
+    }
+
+    public Audience(Pass pass, Long money){
+        this.pass = pass;
+        this.money = money;
+    }
+
+    public boolean hasInvitation() {
+        return ( this.pass != null &&
+            this.pass.getClass().getName().equals("Invitation")
+                );
+    }
+    public boolean hasTicket() {
+
+        return (this.pass != null &&
+            this.pass.getClass().getName().equals("Ticket")
+        );
+    }
+    public void setPass(Pass pass) {
+        this.pass = pass;
+    }
+    public Pass getPass() { return this.pass; }
+
+    public Long getMoney() {
+        return this.money;
+    }
+    public boolean minusMoney(long money) {
+        if(money > this.money) {
+            return false;
+        }
+        this.money -= money;
+        return true;
+    }
+    public void plusMoney(long money) {
+        this.money += money;
+    }
 }
