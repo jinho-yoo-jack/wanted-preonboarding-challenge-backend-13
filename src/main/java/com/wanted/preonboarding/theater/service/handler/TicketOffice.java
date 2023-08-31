e24749757cfb1ea2ef758c2ff1d6a94bfe73c6d2
leekyuwon <kyuwoon369@gmail.com>
2023-08-31 19:57:34 +0900

    티켓 부스 기본 정보를 설정하라

--- src/main/java/com/wanted/preonboarding/theater/service/handler/TicketOffice.java
+++ src/main/java/com/wanted/preonboarding/theater/service/handler/TicketOffice.java
@@ -7,18 +7,24 @@ public class TicketOffice {
     private long amount;
     private final List<Ticket> tickets;
 
-    public TicketOffice(Long amount, Ticket ... tickets) {
+    public TicketOffice(Long amount, Ticket... tickets) {
         this.amount = amount;
         this.tickets = Arrays.asList(tickets);
     }
 
-    public Ticket getTicket(){
+    public TicketOffice() {
+        this.amount = 20000L;
+        this.tickets = Arrays.asList(new Ticket(100L));
+    }
+
+    public Ticket getTicket() {
         return tickets.get(0);
     }
 
     public void minusAmount(long amount) {
         this.amount -= amount;
     }
+
     public void plusAmount(long amount) {
         this.amount += amount;
     }
