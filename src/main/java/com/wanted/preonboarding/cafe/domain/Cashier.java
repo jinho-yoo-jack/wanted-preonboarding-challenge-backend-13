ce5a35b4461152439938b2f4ba871f1194c8d81e
zinzoddari <zinzoddari@gmail.com>
2023-09-02 19:25:58 +0900

    chore: 주석 추가

--- src/main/java/com/wanted/preonboarding/cafe/domain/Cashier.java
+++ src/main/java/com/wanted/preonboarding/cafe/domain/Cashier.java
@@ -4,6 +4,9 @@ import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 
 public class Cashier {
+    /**
+     * 주문에 대한 총액 계산을 합니다.
+     */
     public long calculateTotalPrice(List<Order> orders) {
         AtomicLong totalPrice = new AtomicLong(0L);
         orders.forEach(order -> {
@@ -12,6 +15,9 @@ public class Cashier {
         return totalPrice.get();
     }
 
+    /**
+     * 캐셔는 바리스타의 상황을 판단하여 메뉴 제조를 요청합니다.
+     */
     public String sendTo(Barista barista, List<Order> receivedOrders) {
         if (!barista.isBusy()) {
             return barista.makeCoffeeTo(receivedOrders);
--- src/main/java/com/wanted/preonboarding/cafe/service/CafeService.java
+++ src/main/java/com/wanted/preonboarding/cafe/service/CafeService.java
@@ -10,10 +10,16 @@ import org.springframework.stereotype.Service;
 public class CafeService {
     private final Cafe wantedCafe;
 
+    /**
+     * 인사를 합니다.
+     */
     public String greet() {
         return wantedCafe.welcomeMessage();
     }
 
+    /**
+     * 주문을 접수 받고 처리합니다.
+     */
     public String orderFrom(OrderRequest orderRequest) {
         return wantedCafe.orderReception(orderRequest);
     }
