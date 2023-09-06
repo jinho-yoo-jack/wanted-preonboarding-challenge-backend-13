ff9429ce80131d66c92e7464ce1c167665340b48
이한솔 <hansol8701@naver.com>
2023-09-06 22:12:59 +0900

    refactor: 웰컴 메시지를 상수화

--- src/main/java/com/wanted/preonboarding/cafe/controller/CafeController.java
+++ src/main/java/com/wanted/preonboarding/cafe/controller/CafeController.java
@@ -12,15 +12,16 @@ import java.util.HashMap;
 @RequestMapping("/cafe")
 @RequiredArgsConstructor
 public class CafeController {
+    public static final String WELCOME_MESSAGE = "Welcome to The Wanted coding cafe!!";
     private final CafeService cafeService;
 
     @GetMapping("hello")
-    public String welcomeMessage(){
-        return "Welcome to The Wanted coding cafe!!";
+    public String welcomeMessage() {
+        return WELCOME_MESSAGE;
     }
 
     @GetMapping("order")
-    public String orderFromMenu(){
+    public String orderFromMenu() {
         HashMap<String, Integer> menu = new HashMap<String, Integer>();
         menu.put("AMERICANO", 3);
         return cafeService.orderFrom(menu);
