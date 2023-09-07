811304f122d09816437d6ef699b7c6aae716b728
yeseong31 <yeseong31@naver.com>
2023-09-07 14:19:48 +0900

    refactor: Theater 코드 정리 및 결합도 감소

--- src/main/java/com/wanted/preonboarding/theater/service/handler/Theater.java
+++ src/main/java/com/wanted/preonboarding/theater/service/handler/Theater.java
@@ -6,16 +6,10 @@ import org.springframework.stereotype.Component;
 @Component
 @RequiredArgsConstructor
 public class Theater {
-
-    public void enter(Audience audience, TicketSeller ticketSeller){
-        if(audience.getBag().hasInvitation()){
-            Ticket ticket = ticketSeller.getTicketOffice().getTicket();
-            audience.getBag().setTicket(ticket);
-        }else {
-            Ticket ticket = ticketSeller.getTicketOffice().getTicket();
-            audience.getBag().minusAmount(ticket.getFee());
-            ticketSeller.getTicketOffice().plusAmount(ticket.getFee());
-            audience.getBag().setTicket(ticket);
-        }
+    
+    public void enter(Audience audience, TicketSeller ticketSeller) {
+        
+        long ticketFee = ticketSeller.sellTo(audience);
+        ticketSeller.receivePay(ticketFee);
     }
 }
