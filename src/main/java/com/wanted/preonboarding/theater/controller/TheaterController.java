13a25e744cc598f6e967cf23ab4262740fffa77e
yeseong31 <yeseong31@naver.com>
2023-09-07 14:20:45 +0900

    chore: TheaterController 코드 정리 및 로그 작성

--- src/main/java/com/wanted/preonboarding/theater/controller/TheaterController.java
+++ src/main/java/com/wanted/preonboarding/theater/controller/TheaterController.java
@@ -1,27 +1,28 @@
 package com.wanted.preonboarding.theater.controller;
 
-import com.wanted.preonboarding.cafe.service.CafeService;
 import com.wanted.preonboarding.theater.service.TheaterService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashMap;
-
+@Slf4j
 @RestController
 @RequestMapping("/theater")
 @RequiredArgsConstructor
 public class TheaterController {
+    
     private final TheaterService theaterService;
-
+    
     @GetMapping("hello")
-    public String welcomeMessage(){
+    public String welcomeMessage() {
         return "Welcome to The Wanted Theater";
     }
-
+    
     @GetMapping("enter")
-    public String enter(){
+    public String enter() {
+        log.info("소극장 입장");
         return theaterService.enter();
     }
 }
